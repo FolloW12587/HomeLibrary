35b87841086bffc3393e15a3ce8cc513f43798ff
follow <follow12587@gmail.com>
2024-05-12 13:35:08 +1000

    Initial Commit

---
+++ HomeLibrary/HomeLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeLibraryApp.swift
+//  HomeLibrary
+//
+//  Created by Сергей Дубовой on 12.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
